d926f657d0c08b72718e7f772c84f5c85a520301
0x1zen <rajdubalwork@gmail.com>
2025-05-05 20:29:06 +0530

    Tested a few apis

--- src/app.js
+++ src/app.js
@@ -4,6 +4,22 @@ const app=express();
 
 const PORT=3000;
 
+app.get("/user",(req,res)=>{
+    res.send("User Data");
+})
+app.post("/user",(req,res)=>{
+    res.send("status 200");
+})
+app.patch("/user",(req,res)=>{
+    res.send("Patched User Data");
+})
+app.put("/user",(req,res)=>{
+    res.send("updated user data");
+})
+app.delete("/user",(req,res)=>{
+    res.send("deleted user data");
+})
+
 app.use("/test",(req,res)=>{
     res.send("hello from test route");
 })
