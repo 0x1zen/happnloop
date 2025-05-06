03d1671ea2ace3d5dd77641fe8805cedcf69b04e
0x1zen <rajdubalwork@gmail.com>
2025-05-06 19:02:20 +0530

    Error Handling

--- src/app.js
+++ src/app.js
@@ -2,76 +2,24 @@ const express=require("express");
 
 const app=express();
 
-const {adminAuth,userAuth}= require("./middleware/auth.js")
-
 const PORT=3000;
 
-// admin routes
-
-
-
-app.use("/admin",adminAuth);
-
-app.get("/admin/getAllData",(req,res)=>{
-    res.status(200).send("All Data");
+app.get("/getUserData",(a,b,c)=>{
+    // try{
+    //     throw new Error("Error");
+    // }
+    // catch(e){
+    //     console.log(e.message);
+    //     b.send("Error occured");
+    // }
+    throw new Error("Hey error occured");
+    
 })
 
-app.get("/user/login",(req,res)=>{
-    res.send("user logged in");
-})
-
-app.use("/user",userAuth);
-
-app.get("/user",(req,res)=>{
-    res.send("User Data");
-})
-
-app.post("/user",(req,res)=>{
-    res.send("status 200");
-})
-app.patch("/user",(req,res)=>{
-    res.send("Patched User Data");
-})
-app.put("/user",(req,res)=>{
-    res.send("updated user data");
-})
-app.delete("/user",(req,res)=>{
-    res.send("deleted user data");
-})
-
-app.use("/test",(req,res)=>{
-    res.send("hello from test route");
-})
-
-app.use("/hello",(req,res)=>{
-    res.send("hello from hello route");
-
-})
-// Dynamic ROutes
-app.get("/abc/:userId/:userName",(req,res)=>{
-    console.log(req.params);
-})
-
-
-// Concept Of Middlewares
-
-app.get("/data",(req,res,next)=>{
-    console.log("First Handler");
-    next();
-    res.send("First Response");
-},(req,res,next)=>{
-    console.log("Second Handler");
-    next();
-    res.send("Second Response");
-},(req,res)=>{
-    console.log("THird Response");
-    res.send("Hey!Gotcha.")
-})
-
-// Advanced Routing Techniques
-
-app.use("/",(req,res)=>{
-    res.send("hello from / route");
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send(err.message);
+    }
 })
 
 app.listen(PORT,()=>{
