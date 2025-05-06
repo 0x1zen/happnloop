425dc3488f237962f2ee3c7479c048c3981dc5bf
0x1zen <rajdubalwork@gmail.com>
2025-05-06 21:34:39 +0530

    Basic Middleware Structure,database connectivity and mongoose models

--- src/app.js
+++ src/app.js
@@ -1,28 +1,16 @@
-const express=require("express");
+const express = require("express");
+const connectDB = require("./config/database");
+const app = express();
 
-const app=express();
+const PORT = 3000;
 
-const PORT=3000;
-
-app.get("/getUserData",(a,b,c)=>{
-    // try{
-    //     throw new Error("Error");
-    // }
-    // catch(e){
-    //     console.log(e.message);
-    //     b.send("Error occured");
-    // }
-    throw new Error("Hey error occured");
-    
-})
-
-app.use("/",(err,req,res,next)=>{
-    if(err){
-        res.status(500).send(err.message);
-    }
-})
-
-app.listen(PORT,()=>{
-    console.log(`Successfully listening on ${PORT}`);
-})
-
+connectDB()
+  .then(() => {
+    console.log("Database Connected Successfully");
+    app.listen(PORT, () => {
+      console.log(`Successfully listening on ${PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.error("Error connecting the database");
+  });
