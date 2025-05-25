9f64490e3d63034c4fc323dae0a7dc8c34cb9b18
Raj Dubal <126544333+0x1zen@users.noreply.github.com>
2025-05-25 12:50:52 +0000

    GET:Connections

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,34 +1,42 @@
 const mongoose = require("mongoose");
-
-const connectionRequestSchema = new mongoose.Schema({
-  fromUserId : {
-    type : mongoose.Schema.Types.ObjectId,
-    required : true
-  },
-  toUserId : {
-    type : mongoose.Schema.Types.ObjectId,
-    required : true
-  },
-  status : {
-    type : String,
-    required : true,
-    enum : {
-      values : ["ignored","interested","accepted","rejected"],
-      message : props => `${props.value} is not supported`
+const User = require("./user");
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // reference to the user collection
+      required: true,
     },
-    default : null
-  }
-}, {timestamps: true })
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: (props) => `${props.value} is not supported`,
+      },
+      default: null,
+    },
+  },
+  { timestamps: true },
+);
 
-connectionRequestSchema.pre("save",function(next){
+connectionRequestSchema.pre("save", function (next) {
   const connectionRequest = this;
   // check if my from userid is same as to userid
-  if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
     throw new Error("You cannot send request to yourself");
   }
   next();
-})
+});
 
-const ConnectionRequest = mongoose.model("ConnectionRequest",connectionRequestSchema);
+const ConnectionRequest = mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema,
+);
 
 module.exports = ConnectionRequest;
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
-const User = require("../models/user");
+
+const USER_SAFE_DATA = "firstName lastName photoUrl";
 
 // Get all the pending connection requests for the logged in user
 userRouter.get("/user/requests", userAuth, async (req, res) => {
@@ -11,12 +12,43 @@ userRouter.get("/user/requests", userAuth, async (req, res) => {
     const requests = await ConnectionRequest.find({
       toUserId: user._id,
       status: "interested",
-    });
+      // }).populate("fromUserId", ["firstName", "lastName", "photoUrl"]);
+    }).populate("fromUserId", "firstName lastName photoUrl");
+
     if (requests) {
       res.status(200).json({ data: requests });
     } else {
       res.status(400).json({ message: "No Requests Found" });
     }
+  } catch (err) {
+    res.status(400).send("Error :" + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connections = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+    console.log(connections);
+    const people = connections.map((connection) => {
+      // console.log(connection);
+      const isSender =
+        connection.fromUserId._id.toString() === loggedInUser._id.toString();
+      const otherUser = isSender ? connection.toUserId : connection.fromUserId;
+      return {
+        _id: otherUser._id,
+        firstName: otherUser.firstName,
+        lastName: otherUser.lastName,
+      };
+    });
+    res.status(200).json({ data: people });
   } catch (err) {
     res.status(400).send("Error :" + err.message);
   }
